66e45bf138025be543cc621ebb43a0c4d91d6eb7
Armand Asnani <aasnani@umass.edu>
2026-02-02 00:41:20 +0700

    test call ssm

--- cmd/bnet-polling-function/main.go
+++ cmd/bnet-polling-function/main.go
@@ -7,7 +7,21 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ssm"
 )
+
+var ssmClient *ssm.Client
+
+func init() {
+	// Initialize the SDK once during the "cold start"
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+	ssmClient = ssm.NewFromConfig(cfg)
+}
 
 func HandleRequest(ctx context.Context, event events.CloudWatchEvent) (string, error) {
 	// 1. First log message (visible)
@@ -22,7 +36,23 @@ func HandleRequest(ctx context.Context, event events.CloudWatchEvent) (string, e
 		return "", err
 	}
 
-	// 2. Convert the byte slice to a string
+	clientIDPath := "/serversup/bnet/client_id"
+
+	// 2. Call SSM for the Client ID
+	out, err := ssmClient.GetParameter(ctx, &ssm.GetParameterInput{
+		Name:           aws.String(clientIDPath),
+		WithDecryption: aws.Bool(true),
+	})
+
+	if err != nil {
+		log.Printf("Couldn't retrieve Battle Net Client Parameter: %v", err)
+		return "", err
+	}
+
+	val := *out.Parameter.Value
+	log.Printf("Successfully fetched Client ID: %s", val)
+
+	// 3. Convert the byte slice to a string
 	jsonString := string(jsonBytes)
 
 	return "Successfully processed event with data: " + jsonString, nil
