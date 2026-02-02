bfd785958f594d16e7983641cbec42dd4e7f3eff
aasnani <38272153+aasnani@users.noreply.github.com>
2026-02-03 00:02:49 +0700

    add s3 call (#16)

--- cmd/bnet-polling-function/main.go
+++ cmd/bnet-polling-function/main.go
@@ -3,16 +3,35 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"io"
 	"log"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/aws-sdk-go-v2/service/ssm"
 )
 
-var ssmClient *ssm.Client
+var (
+	ssmClient *ssm.Client
+	s3Client  *s3.Client
+)
+
+type Config struct {
+	Region                 string  `json:"region"`
+	Locale                 string  `json:"locale"`
+	Realms                 []Realm `json:"realms"`
+	PollingIntervalSeconds int     `json:"polling_interval_seconds"`
+}
+
+type Realm struct {
+	Name string `json:"name"`
+	Slug string `json:"slug"`
+}
 
 func init() {
 	// Initialize the SDK once during the "cold start"
@@ -21,6 +40,7 @@ func init() {
 		log.Fatalf("unable to load SDK config, %v", err)
 	}
 	ssmClient = ssm.NewFromConfig(cfg)
+	s3Client = s3.NewFromConfig(cfg)
 }
 
 func HandleRequest(ctx context.Context, event events.CloudWatchEvent) (string, error) {
@@ -36,21 +56,61 @@ func HandleRequest(ctx context.Context, event events.CloudWatchEvent) (string, e
 		return "", err
 	}
 
-	clientIDPath := "/serversup/bnet/client_id"
+	clientIDParameterPath := os.Getenv("BNET_CLIENT_ID_PATH")
+	clientSecretParameterPath := os.Getenv("BNET_CLIENT_SECRET_PATH")
 
 	// 2. Call SSM for the Client ID
-	out, err := ssmClient.GetParameter(ctx, &ssm.GetParameterInput{
-		Name:           aws.String(clientIDPath),
+	clientIdParameterOutput, err := ssmClient.GetParameter(ctx, &ssm.GetParameterInput{
+		Name:           aws.String(clientIDParameterPath),
 		WithDecryption: aws.Bool(true),
 	})
 
 	if err != nil {
-		log.Printf("Couldn't retrieve Battle Net Client Parameter: %v", err)
+		log.Printf("Couldn't retrieve Battle Net Client ID Parameter: %v", err)
 		return "", err
 	}
 
-	val := *out.Parameter.Value
+	val := *clientIdParameterOutput.Parameter.Value
 	log.Printf("Successfully fetched Client ID: %s", val)
+
+	clientSecretParameterOutput, err := ssmClient.GetParameter(ctx, &ssm.GetParameterInput{
+		Name:           aws.String(clientSecretParameterPath),
+		WithDecryption: aws.Bool(true),
+	})
+
+	_ = clientSecretParameterOutput
+
+	if err != nil {
+		log.Printf("Couldn't retrieve Battle Net Client Secret Parameter: %v", err)
+		return "", err
+	}
+
+	obj, err := s3Client.GetObject(ctx, &s3.GetObjectInput{
+		Bucket: aws.String(os.Getenv("CONFIG_BUCKET")),
+		Key:    aws.String(os.Getenv("BNET_SERVER_CONFIG_PATH")),
+	})
+
+	bodyBytes, err := io.ReadAll(obj.Body)
+	if err != nil {
+		return "", fmt.Errorf("failed to read S3 body: %w", err)
+	}
+	defer obj.Body.Close()
+
+	// 2. Unmarshal into your Config struct (or a map if you're feeling lazy)
+	var cfg Config
+	if err := json.Unmarshal(bodyBytes, &cfg); err != nil {
+		return "", fmt.Errorf("failed to unmarshal config: %w", err)
+	}
+
+	// 3. Marshal it back into a PRETTY byte slice
+	// The "" is the prefix, and "  " (two spaces) is the indent
+	prettyJSON, err := json.MarshalIndent(cfg, "", "  ")
+	if err != nil {
+		return "", fmt.Errorf("failed to generate pretty JSON: %w", err)
+	}
+
+	// 4. Print that masterpiece! 🎨
+	log.Printf("Loaded Config:\n%s", string(prettyJSON))
 
 	// 3. Convert the byte slice to a string
 	jsonString := string(jsonBytes)
